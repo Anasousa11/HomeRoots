e1b5aec58e07e21781b440289e3accc14acc2a37
Ana Sousa <anasousa1100@outlook.com>
2025-11-19 19:30:47 +0000

    add class-based views

--- homeschool_planner/core/views.py
+++ homeschool_planner/core/views.py
@@ -1,5 +1,9 @@
 # Import render and define a view for the homepage
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Student
+from .forms import StudentForm
 
 def index(request):
     return render(request, 'core/index.html')
@@ -11,3 +15,28 @@ def progress(request):
     return render(request, 'core/progress.html')
 def contact(request):
     return render(request, 'core/contact.html')
+class StudentListView(ListView):
+    model = Student
+    template_name = 'core/students/list.html'
+    context_object_name = 'students'
+    paginate_by = 12
+
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = 'core/students/detail.html'
+    context_object_name = 'student'
+
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'core/students/form.html'
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'core/students/form.html'
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = 'core/students/confirm_delete.html'
+    success_url = reverse_lazy('core:students')

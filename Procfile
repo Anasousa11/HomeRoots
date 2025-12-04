39a2552912d9bbf9a595405eb66b3250dbec7778
Ana Sousa <anasousa1100@outlook.com>
2025-12-04 16:34:55 +0000

    add procfile and add heroku to host

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn homeroots.wsgi
--- homeschool_planner/homeschool_planner/settings.py
+++ homeschool_planner/homeschool_planner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7j@w#a4%xkwj0ga8zw-@=hdy(fm1xpcb(n6b(o0+&n-8n3h*3u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
